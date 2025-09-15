69868a385991f8855db07a07a20d308257aa535d
Sergio Martinez <a27299@svalero.com>
2025-09-15 17:32:58 +0200

    Add files via upload

---
+++ Main_191.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main_191 {
+
+  static Scanner in;
+    public static void casoDePrueba() {
+        String[] datos = in.nextLine().split(" ");
+        
+        int s = ((Integer.parseInt(datos[0]) - 1) * Integer.parseInt(datos[0]))/2;
+        
+        System.out.println((Integer.parseInt(datos[1])*Integer.parseInt(datos[0]))-(s*(Integer.parseInt(datos[2]))));
+    } 
+    public static void main(String[] args) {
+        in = new java.util.Scanner(System.in);
+        
+        int numCasos = in.nextInt();
+        in.nextLine();
+        
+        for (int i = 0; i < numCasos; i++)
+            casoDePrueba();
+    }
+}
