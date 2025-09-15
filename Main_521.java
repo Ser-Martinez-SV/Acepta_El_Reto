174119931e1d9b72b3aeee7a03e47e749bb206fb
Sergio Martinez <a27299@svalero.com>
2025-09-15 17:41:45 +0200

    Update Main_521.java

--- Main_521.java
+++ Main_521.java
@@ -1,25 +1,32 @@
 public class Main_521 {
     static java.util.Scanner sc;
     public static void problema(String[] datos){
+        
         int pisos = Integer.parseInt(datos[0]);
         int letras = Integer.parseInt(datos[1]);
         int asistentes = Integer.parseInt(datos[2]);
         int med = 0;
+        
         if ((pisos*letras) % 2 != 0){
             med = (pisos*letras+1)/2;
         }else{
             med = pisos*letras/2;
         }
+        
         String[] asis = sc.nextLine().split(" ");
+        
         if(asistentes >= med){
             int[][] m = new int[pisos][letras];
             int cont = 0;
             char letra = 'A';
+            
             for (int i = 0; i < asis.length; i+=2){
                 letra = asis[i+1].charAt(0);
                 if (m[Integer.parseInt(asis[i])-1][(letra-'0')-17] == 0){
+                    
                     m[Integer.parseInt(asis[i])-1][(letra-'0')-17] = 1;
                     cont++;
+                    
                 }
             }
             if (cont >= med){
@@ -34,6 +41,7 @@ public class Main_521 {
     public static void main(String[] args) {
         sc = new java.util.Scanner(System.in);
         String[] datos = sc.nextLine().split(" ");
+        
         while (!(datos[0].equals("0") && datos[1].equals("0") && datos[1].equals("0"))){
             problema(datos);
             datos = sc.nextLine().split(" ");
