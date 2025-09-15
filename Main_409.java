537b775ad72a22ccbc983e734d162f89a26c3871
Sergio Martinez <a27299@svalero.com>
2025-09-15 17:43:13 +0200

    Update Main_409.java

--- Main_409.java
+++ Main_409.java
@@ -1,20 +1,27 @@
 public class Main_409 {
+    
     static java.util.Scanner sc;
     static java.util.Arrays Arrays;
+    
     public static int[] ordenar(String[] pers){
+        
         int [] new_pers = new int[pers.length];
+        
         for (int i = 0; i < pers.length; i++) {
             new_pers[i] = Integer.parseInt(pers[i]);
         }
+        
         java.util.Arrays.sort(new_pers);
         return new_pers;
     }
     public static void problema(){
+        
         String[] datos = sc.nextLine().split(" ");
         int caste  = Integer.parseInt(datos[0]);
         int[] personas = ordenar(sc.nextLine().split(" "));
         int cont = 0;
         int comprobador = -1;
+        
         for (int i = 0; i < (personas.length - caste + 1); i++) {
             if ( comprobador < i && ((personas[i] + 15) >= personas[i+caste-1])){
                 cont++;
@@ -25,9 +32,12 @@ public class Main_409 {
 
     }
     public static void main(String[] args) {
+        
         sc = new java.util.Scanner(System.in);
+        
         int n_p = sc.nextInt();
         sc.nextLine();
+        
         for (int i = 0; i < n_p; i++) {
             problema();
         }
